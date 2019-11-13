5fb6df87bb9071b060d7b64ccf3ce4d90f38a2fe
Janick Norman <janicknorman@gmail.com>
2019-11-13 14:19:19 +0700

    PROXY

--- app.js
+++ app.js
@@ -22,13 +22,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/waproxy', proxy('con-rokitvhelp.zendesk.com', {
-  https: true
-}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jendek', jendekRouter);
-app.use('/px', proxyRouter);
+app.use('/wacoreproxy', proxy('192.168.29.189:9001')); 
+app.use('/cifherokuproxy/:appname', proxy(req => req.params["appname"] + ".herokuapp.com", {https: true}))
+app.use('/cifngrokproxy/:appname', proxy(req => req.params["appname"] + ".ngrok.io", {https: true}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
