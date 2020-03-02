c6b2724ea60c9d1abf581c65db7feea1c6ba95fc
Janick Norman <janicknorman@gmail.com>
2020-03-02 16:40:48 +0700

    hapus debug error

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  logger.debug(err)
   logger.error(err.stack)
   // render the error page
   res.status(err.status || 500);
